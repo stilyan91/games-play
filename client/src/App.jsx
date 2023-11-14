c8fdc351c37d67a77a1a91b88899cf04b04b9522
stilyan91 <stilyan91@gmail.com>
2023-11-14 20:04:13 +0200

    add styles and Header-component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
-
+import Header from './components/Header/Header';
 
 function App() {
 
 
   return (
-    <>
+    <div id="box">
 
-    </>
+      <Header />
+
+
+    </div>
   )
 }
 
