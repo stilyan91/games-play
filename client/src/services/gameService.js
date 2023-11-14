a08dc51b446f7691a82778f94baad800524112d2
stilyan91 <stilyan91@gmail.com>
2023-11-14 21:32:14 +0200

    create gameService

---
+++ client/src/services/gameService.js
@@ -0,0 +1,14 @@
+const base_url = 'http://localhost:3030/jsonstore'
+
+export const create = async (gameData) => {
+    const response = await fetch(`${base_url/games}`,{
+                        method: "POST",
+                        headers: {
+                            'content-type': 'application/json',
+                        },
+                        body: JSON.stringify(gameData)
+                    });
+
+    const result = await response.json();
+    return result;
+};
