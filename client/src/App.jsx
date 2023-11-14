9df095aac2019f6008d56b4333a4504d15271698
stilyan91 <stilyan91@gmail.com>
2023-11-14 20:59:26 +0200

    add Games Catalog

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,10 @@
-import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+
+
 import Header from './components/Header/Header';
+import Home from './components/home/Home';
+import GameList from './components/game-list/GameList';
 
 function App() {
 
@@ -9,8 +14,18 @@ function App() {
 
       <Header />
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/games" element={<GameList />} />
+        <Route path="/games/create" element={<CreateGame />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<Register />} />
 
-    </div>
+      </Routes >
+
+
+    </div >
   )
 }
 
