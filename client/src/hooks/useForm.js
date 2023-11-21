cb0a96a8301de13fd443fbec4738850161a3fd52
stilyan91 <stilyan91@gmail.com>
2023-11-21 22:36:34 +0200

    change login to comtrolled

---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+export default function useForm(submitHandler, initValues) {
+    const [values, setValues] = useState(initValues);
+
+    const onChange = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+
+        }));
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        submitHandler(values)
+    };
+
+    return {
+        values,
+        onChange,
+        onSubmit
+    }
+};
